4caf5f156f743bc6e43bd068ad416f3ca668945c
nadav <tashernadav@gmail.com>
2017-09-22 13:53:21 -0400

    Permissions: Added Internet Permission For App.

--- app/src/main/java/zuk/nadav/testedapp4/MainActivity.java
+++ app/src/main/java/zuk/nadav/testedapp4/MainActivity.java
@@ -2,12 +2,14 @@ package zuk.nadav.testedapp4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout ll=findViewById(R.id.linearLayout1);
         setContentView(R.layout.activity_main);
     }
 }
