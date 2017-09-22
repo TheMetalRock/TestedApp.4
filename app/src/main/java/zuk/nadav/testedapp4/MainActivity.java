58cecd192a7570bd24a5cce0b1516eb53a4598fc
nadav <tashernadav@gmail.com>
2017-09-22 13:54:33 -0400

    Added Code For Screen Size Detection

--- app/src/main/java/zuk/nadav/testedapp4/MainActivity.java
+++ app/src/main/java/zuk/nadav/testedapp4/MainActivity.java
@@ -3,13 +3,17 @@ package zuk.nadav.testedapp4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import nadav.tasher.lightool.Light;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout ll=findViewById(R.id.linearLayout1);
+//        LinearLayout ll=findViewById(R.id.linearLayout1);
+        Toast.makeText(this,"Screen: "+ Light.Device.screenX(getApplicationContext()) + "x"+ Light.Device.screenY(this),Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
     }
 }
